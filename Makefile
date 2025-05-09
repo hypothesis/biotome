4adca721de439ba2cf293696fd7b57471a00f8b9
Sean Hammond <seanh@users.noreply.github.com>
2025-05-09 15:16:20 +0100

    Fix the local dev server

--- Makefile
+++ Makefile
@@ -4,6 +4,5 @@ help:
 	@echo "make dev               Run the site in the development server"
 
 .PHONY: dev
-dev: HYPOTHESIS_EMBED_URL := http://localhost:5000/embed.js
 dev:
 	find src | entr -r bin/run.sh
--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+HYPOTHESIS_EMBED_URL=http://localhost:5000/embed.js
+export HYPOTHESIS_EMBED_URL
+
 bin/build.sh
 python3 -m http.server -b 127.0.0.1 47291 -d build
