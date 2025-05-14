48fb75ba2862f93b1a8f3ab1cda2d297f790ee1b
Sean Hammond <seanh@users.noreply.github.com>
2025-05-14 18:29:00 +0100

    Restrict groups in client

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 
 HYPOTHESIS_EMBED_URL=http://localhost:5000/embed.js
-export HYPOTHESIS_EMBED_URL
+HYPOTHESIS_API_URL=http://localhost:5000/api/
+HYPOTHESIS_AUTHORITY=localhost
+HYPOTHESIS_GROUPS='"Pi3Pmdmm", "6279Y8xA"'
+export HYPOTHESIS_EMBED_URL HYPOTHESIS_API_URL HYPOTHESIS_AUTHORITY HYPOTHESIS_GROUPS
 
 bin/build.sh
 python3 -m http.server -b 127.0.0.1 47291 -d build
