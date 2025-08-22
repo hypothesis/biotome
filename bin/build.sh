f84a347aa0bb58eb1655aae09d501170febd29e7
Sean Hammond <seanh@users.noreply.github.com>
2025-08-22 15:02:03 +0100

    Revert "Restrict groups in client"
    This reverts commit 48fb75ba2862f93b1a8f3ab1cda2d297f790ee1b.

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 mkdir -p build
-envsubst '${HYPOTHESIS_EMBED_URL},${HYPOTHESIS_API_URL},${HYPOTHESIS_AUTHORITY},${HYPOTHESIS_GROUPS}' < src/index.html > build/index.html
+envsubst '${HYPOTHESIS_EMBED_URL}' < src/index.html > build/index.html
 cp -r src/assets build
--- bin/run.sh
+++ bin/run.sh
@@ -2,10 +2,7 @@
 set -euo pipefail
 
 HYPOTHESIS_EMBED_URL=http://localhost:5000/embed.js
-HYPOTHESIS_API_URL=http://localhost:5000/api/
-HYPOTHESIS_AUTHORITY=localhost
-HYPOTHESIS_GROUPS='"Pi3Pmdmm", "6279Y8xA"'
-export HYPOTHESIS_EMBED_URL HYPOTHESIS_API_URL HYPOTHESIS_AUTHORITY HYPOTHESIS_GROUPS
+export HYPOTHESIS_EMBED_URL
 
 bin/build.sh
 python3 -m http.server -b 127.0.0.1 47291 -d build
