11b618c84a2adc63014aa42a60fcf76ca3b39571
Alejandro Celaya <alejandro.celaya@hypothes.is>
2025-10-30 15:17:18 +0100

    Enable groups allowlist setting

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-HYPOTHESIS_EMBED_URL=http://localhost:5000/embed.js
-export HYPOTHESIS_EMBED_URL
+export HYPOTHESIS_EMBED_URL=http://localhost:5000/embed.js
+export HYPOTHESIS_GROUPS_ALLOWLIST="__world__"
 
 bin/build.sh
 python3 -m http.server -b 127.0.0.1 47291 -d build
