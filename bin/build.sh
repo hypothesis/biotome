09c87b014cad5d02a6b7382bdcd85bb049fd71af
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-10-30 15:19:18 +0100

    Enable groups allowlist setting (#12)

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 mkdir -p build
-envsubst '${HYPOTHESIS_EMBED_URL}' < src/index.html > build/index.html
+envsubst '${HYPOTHESIS_EMBED_URL} ${HYPOTHESIS_GROUPS_ALLOWLIST}' < src/index.html > build/index.html
 cp -r src/assets build
